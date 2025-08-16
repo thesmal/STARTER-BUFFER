7ced4e4a4b391b58b65efc539f8e3463bc1ac9fa
thesmal <132695127+thesmal@users.noreply.github.com>
2025-08-16 11:53:30 +0500

    Update main.js
    added some stuff for erekir

--- scripts/main.js
+++ scripts/main.js
@@ -17,36 +17,56 @@ UnitTypes.alpha.itemCapacity = 99999
 UnitTypes.alpha.mineSpeed = 999999
 UnitTypes.alpha.buildSpeed = 999999
 
+// Make evoke OP
+UnitTypes.evoke.health = 99999
+UnitTypes.evoke.range = 200
+UnitTypes.evoke.mineTier = 100
+UnitTypes.evoke.itemCapacity = 99999
+UnitTypes.evoke.mineSpeed = 999999
+UnitTypes.evoke.buildSpeed = 999999
+
+// Stell
+UnitTypes.stell.weapons.get(0).bullet = fireball
+UnitTypes.stell.health = 99999
+UnitTypes.stell.range = 200
+
 // Core upgrade
 Blocks.coreShard.health = 99999
 Blocks.coreShard.itemCapacity = 99999
+Blocks.coreBastion.health = 99999
+Blocks.coreBastion.itemCapacity = 99999
 
-//Duo & scatter
+// Duo & Scatter & Breach
 Blocks.duo.ammoTypes.get(Items.copper).damage = 999999
 Blocks.duo.health = 999999
 Blocks.scatter.targetGround = true
 Blocks.scatter.health = 999999
 Blocks.scatter.ammoTypes.get(Items.lead).collidesGround = true
+Blocks.breach.ammoTypes.get(Items.beryllium).damage = 999999
+Blocks.breach.health = 999999
 
-// wol
+// Walls
 Blocks.copperWall.health = 999999
+Blocks.berylliumWall.health = 999999
 
-//drill
+// Mechanical Drill
 Blocks.mechanicalDrill.health = 999999
 Blocks.mechanicalDrill.drillTime = 1
 Blocks.mechanicalDrill.tier = 100
 
-//conveyor
+// Plasma bore
+Blocks.plasmaBore.health = 999999
+Blocks.plasmaBore.drillTime = 1
+
+// Conveyors
 Blocks.conveyor.speed = 999999
 Blocks.conveyor.displayedSpeed = 999999
 Blocks.conveyor.itemCapacity = 15
 Blocks.conveyor.health = 666666
+Blocks.duct.itemCapacity = 30
+Blocks.duct.health = 666666
 
-//other junk
+// CREATIVE MODE STUFFFFFF
 Blocks.itemSource.buildVisibility = BuildVisibility.shown
 Blocks.payloadSource.buildVisibility = BuildVisibility.shown
 Blocks.liquidSource.buildVisibility = BuildVisibility.shown
-Blocks.stoneWall.destructible = true
-Blocks.stoneWall.buildVisibility = BuildVisibility.shown
-
-
